88865ec7f61659faa9d9b82b8cc2935a4708d41d
FonzyLima <71457986+FonzyLima@users.noreply.github.com>
2021-12-26 23:14:39 +0800

    CHATBOT

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,31 @@
-import '../styles/globals.css'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'boxicons/css/boxicons.css';
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "boxicons/css/boxicons.css";
 import * as React from "react";
 
-import Navbar from '../components/Navbar'
-import Contact from '../components/Contact';
+import Navbar from "../components/Navbar";
+import Contact from "../components/Contact";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
+import MessengerCustomerChat from "react-messenger-customer-chat";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-    <head>
-        <meta charSet="UTF-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        
-        
-        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossOrigin="anonymous"/>
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-
-        
-
-    </head>
-    <Navbar/>
-    <Component {...pageProps} />
-    <ToastContainer
+        <link
+          rel="stylesheet"
+          href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+          integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
+          crossOrigin="anonymous"
+        />
+      </head>
+      <Navbar />
+      <Component {...pageProps} />
+      <ToastContainer
         position="top-right"
         autoClose={8000}
         hideProgressBar={false}
@@ -33,11 +35,14 @@ function MyApp({ Component, pageProps }) {
         closeOnClick
         pauseOnHover
       />
-    <Contact/>
+      <MessengerCustomerChat
+        pageId="107801160795209"
+        appId="1524224601298273"
+      />
+      ,
+      <Contact />
     </>
-    
-
-  ) 
+  );
 }
 
-export default MyApp
+export default MyApp;
