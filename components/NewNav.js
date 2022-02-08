db0c70b36157389b9f395f33017c635e70dbbb7c
Alfonso Lima <Alfonso_lima@dlsu.edu.ph>
2022-02-08 22:32:02 +0800

    Update NewNav.js

--- components/NewNav.js
+++ components/NewNav.js
@@ -9,7 +9,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
         <Navbar id="navbar" className="navbar navbar-expand-lg sticky-top" expand="lg">
         <Navbar.Brand href="/"><img className="navpicture" src="navlogo.png"/></Navbar.Brand>
         <Navbar.Toggle className="navbar-toggler custom-toggler" aria-controls="navbarSupportedContent" />
-        <Navbar.Collapse id="collapse navbar-collapse" id="navbarSupportedContent">
+        <Navbar.Collapse id="collapse navbar-collapse" aria-controls="navbarSupportedContent">
             <Nav className="navbar-nav me-auto mb-2 mb-lg-0">
                 <Nav.Link className="nav-item" href="/">Home</Nav.Link>
                 <Nav.Link href="/properties">Properties</Nav.Link>
